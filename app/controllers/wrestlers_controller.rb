c29511b54c0247da6fde82fc42a4f8917d0a78f7
johnrusch <johnrusch7@gmail.com>
2020-07-19 13:55:43 -0700

    adds api service, reorganizes controllers and adds api namespacing, configures routes

--- app/controllers/wrestlers_controller.rb
+++
@@ -1,2 +0,0 @@
-class WrestlersController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  resources :matches
-  resources :wrestlers
-  resources :teams
-  resources :leagues
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :matches
+      resources :wrestlers
+      resources :teams
+      resources :leagues
+      resources :users
+      post '/auth', to:
+      'auth#create'
+      get '/current_user', to:
+      'auth#show'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
