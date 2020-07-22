a1c59b8952ec9d88f8e0b7bab7693ee3688adf52
johnrusch <johnrusch7@gmail.com>
2020-07-21 22:44:16 -0700

    adds signup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::UsersController < ApplicationController
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid? && @user.authenticate(params[:password])
+            token = issue_token(@user)
+            render json: {id: @user.id, name: @user.name, jwt: token}
+        else
+            render json: { error: 'Failed to create user' }, status: :not_acceptable
+        end
+    end
+
+    private
+    def user_params
+        params.permit(:name, :password)
+    end
 end
