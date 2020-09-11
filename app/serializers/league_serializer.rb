e271e035a8e5354bc5539b9baa9b4a000e6c3a87
johnrusch <johnrusch7@gmail.com>
2020-09-11 16:43:34 -0700

    adds closed and creator id attributes to league serializer

--- app/serializers/league_serializer.rb
+++ app/serializers/league_serializer.rb
@@ -1,6 +1,6 @@
 class LeagueSerializer < ActiveModel::Serializer
   has_many :teams, serializer: TeamSerializer, include_nested_associations: true
-  attributes :id, :name
+  attributes :id, :name, :closed, :creator_id
 
   # def teams
   #   self.object.teams.map do |team|
