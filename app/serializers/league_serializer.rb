135a120d5068396ffe39a216c47ed4054c4f2a54
johnrusch <johnrusch7@gmail.com>
2020-10-29 19:27:41 -0700

    adds users to league serializer

--- app/serializers/league_serializer.rb
+++ app/serializers/league_serializer.rb
@@ -1,5 +1,6 @@
 class LeagueSerializer < ActiveModel::Serializer
   has_many :teams, serializer: TeamSerializer, include_nested_associations: true
+  has_many :users
   attributes :id, :name, :closed, :creator_id
 
   # def teams
