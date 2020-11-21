612fa20c1eda10eb2f2eab511a5c26f6757f761a
johnrusch <johnrusch7@gmail.com>
2020-11-20 16:07:54 -0800

    ok maybe the routes are the problem?

--- config/routes.rb
+++ config/routes.rb
@@ -10,15 +10,10 @@ Rails.application.routes.draw do
       'auth#create'
       get '/current_user', to:
       'auth#show'
-      get '/banzuke', to:
-      'wrestlers#banzuke'
       get '/user_leagues', to:
       'leagues#user_leagues'
       get '/open_leagues', to:
       'leagues#open_leagues'
-      mount ActionCable.server => '/cable'
-      patch '/leagues/:id', to:
-      'leagues#add_user_to_league'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
