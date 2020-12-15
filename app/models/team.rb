87deb752cf074cda4044ee054faba193f3a453cd
johnrusch <johnrusch7@gmail.com>
2020-12-15 10:46:09 -0800

    sets points for a newly initialized team to 0

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,5 +2,10 @@ class Team < ApplicationRecord
   belongs_to :user
   belongs_to :league
   has_and_belongs_to_many :wrestlers
+  after_initialize :init
 
+  def init
+    self.points ||= 0
+  end
+  
 end
