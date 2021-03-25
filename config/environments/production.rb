d2a4d7497b2460fb3d23cbd0ff0a678ed24e87fb
johnrusch <johnrusch7@gmail.com>
2021-03-25 15:19:51 -0700

    sets allowed request origins for production action cable to local host for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://fantasy-sumo-backend.herokuapp.com/api/v1/cable'
-  config.action_cable.allowed_request_origins = [ 'http://fantasy-sumo.netlify.app', /http:\/\/fantasy-sumo.netlify.app.*/ ]
+  config.action_cable.allowed_request_origins = [ /http:\/\/localhost:*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
