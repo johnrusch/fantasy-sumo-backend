41c7d71084b38bfa51f63498b4b780b135d7365f
johnrusch <johnrusch7@gmail.com>
2021-04-21 22:11:57 -0700

    shoot

--- app/channels/leagues_channel.rb
+++ app/channels/leagues_channel.rb
@@ -1,18 +1,19 @@
 class LeaguesChannel < ApplicationCable::Channel
-  periodically every: 1.second do
-    transmit payload: @t.total_time
-  end
-
-  @t = Timer.new
+  # periodically every: 1.second do
+  #   transmit payload: @t.total_time
+  # end
 
     def subscribed
       @league = League.find_by(id: params[:leagueID])
-      @t.start
       stream_for @league
     end
 
     def received(data)
         LeaguesChannel.broadcast_to(@league)
+    end
+
+    def start_draft
+      
     end
   
     def unsubscribed
