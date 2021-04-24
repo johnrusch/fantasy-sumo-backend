b52b5b70de860e1ef7ddec9450ab371db33bcff1
johnrusch <johnrusch7@gmail.com>
2021-04-23 20:44:52 -0700

    adds subscription method and periodic timer for draft channel

--- app/channels/drafts_channel.rb
+++ app/channels/drafts_channel.rb
@@ -1,6 +1,11 @@
 class DraftsChannel < ApplicationCable::Channel
+  periodically every: 1.second do
+    transmit payload: "ding from drafts"
+  end
+
   def subscribed
-    
+    @league = League.find_by(id: params[:id])
+    stream_for @league
   end
 
   def unsubscribed
