56ce11ce02bc2246286984595235dd9d483ba526
johnrusch <johnrusch7@gmail.com>
2021-04-27 11:12:35 -0700

    adds new scheduler when drafts channel is subscribes, was making new scheduler every time button was pressed and running all of them

--- app/channels/drafts_channel.rb
+++ app/channels/drafts_channel.rb
@@ -2,22 +2,22 @@ class DraftsChannel < ApplicationCable::Channel
   
 
   def subscribed
+    @timer = Rufus::Scheduler.new
     stream_from "drafts_channel"
   end
 
   def start_timer
     time_remaining = 7
-      timer = Rufus::Scheduler.new
-        timer.every '1s' do |job|
+    @timer.every '1s' do |job|
           # DraftsChannel.broadcast_to(@league, {
           #   draftClock: Time.at(time_remaining).utc.strftime("%M:%S")
           # })
-          ActionCable.server.broadcast "drafts_channel", draftClock: Time.at(time_remaining).utc.strftime("%M:%S")
-          time_remaining -= 1
-          if time_remaining < 0
-            job.unschedule
-          end
-        end
+      ActionCable.server.broadcast "drafts_channel", draftClock: Time.at(time_remaining).utc.strftime("%M:%S")
+      time_remaining -= 1
+      if time_remaining < 0
+        job.unschedule
+      end
+    end
   end
 
   def unsubscribed
