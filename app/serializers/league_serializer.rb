7a442c33f9e18faffda3d016bf0a76417b76a9aa
johnrusch <johnrusch7@gmail.com>
2021-04-28 11:28:26 -0700

    removes team shuffler from leagues serializer

--- app/serializers/league_serializer.rb
+++ app/serializers/league_serializer.rb
@@ -3,18 +3,18 @@ class LeagueSerializer < ActiveModel::Serializer
   has_many :users
   attributes :id, :name, :closed, :creator_id
 
-  def teams
-    unshuffledTeams = self.object.teams.map do |team|
-    {
-    id: team.id,
-    name: team.name,
-    user: team.user,
-    points: team.points,
-    wrestlers: team.wrestlers
-    }
-    end
-    unshuffledTeams.shuffle
-  end
+  # def teams
+  #   unshuffledTeams = self.object.teams.map do |team|
+  #   {
+  #   id: team.id,
+  #   name: team.name,
+  #   user: team.user,
+  #   points: team.points,
+  #   wrestlers: team.wrestlers
+  #   }
+  #   end
+  #   unshuffledTeams.shuffle
+  # end
 
   
 end
