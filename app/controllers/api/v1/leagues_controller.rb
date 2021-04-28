8496bcee8a5d29a1b9dd1a2af0f248a1652f2f04
johnrusch <johnrusch7@gmail.com>
2021-04-28 11:45:42 -0700

    hmm idk

--- app/controllers/api/v1/leagues_controller.rb
+++ app/controllers/api/v1/leagues_controller.rb
@@ -51,13 +51,7 @@ class Api::V1::LeaguesController < ApplicationController
     def start_draft
         league = League.all.find {|league| league.id == league_params[:leagueID]}
         if league
-            # league.closed = true
-            league.save
             shuffled_teams = league.teams.shuffle
-            # LeaguesChannel.broadcast_to(league, {
-            #     shuffledTeams: shuffled_teams,
-            #     draftStarted: true
-            # })
             render json: {
                 shuffledTeams: shuffled_teams,
                 draftStarted: true
