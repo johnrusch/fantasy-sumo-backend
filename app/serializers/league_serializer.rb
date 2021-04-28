c5672dad455b494b8fdb51fdc8423055231b58f3
johnrusch <johnrusch7@gmail.com>
2021-04-27 21:10:53 -0700

    adds shuffled teams to league serializer

--- app/serializers/league_serializer.rb
+++ app/serializers/league_serializer.rb
@@ -3,19 +3,18 @@ class LeagueSerializer < ActiveModel::Serializer
   has_many :users
   attributes :id, :name, :closed, :creator_id
 
-  # def teams
-  #   self.object.teams.map do |team|
-  #     {
-  #     id: team.id,
-  #     name: team.name,
-  #     user: team.user,
-  #     points: team.points,
-  #     wrestlers: team.wrestlers
-  #     }
-  #   end
-
-
-  # end
+  def teams
+      self.object.teams.map do |team|
+      {
+      id: team.id,
+      name: team.name,
+      user: team.user,
+      points: team.points,
+      wrestlers: team.wrestlers
+      }
+    end.shuffle
+    
+  end
 
   
 end
