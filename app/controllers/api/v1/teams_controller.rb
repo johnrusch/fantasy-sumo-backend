15c42d67f561555ba6675e692070faea4d56ac53
johnrusch <johnrusch7@gmail.com>
2021-07-07 10:57:26 -0700

    adds stronger params to teams controller

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -6,12 +6,12 @@ class Api::V1::TeamsController < ApplicationController
     end
 
     def update
-        team = Team.all.find { |team| team.id == params.teamID }
-        wrestler = Wrestler.all.find { |wrestler| wrestler.id == params.wrestlerID }
+        team = Team.all.find { |team| team.id == team_params[:teamID] }
+        wrestler = Wrestler.all.find { |wrestler| wrestler.id == team_params[:wrestlerID] }
         team.wrestlers << wrestler
         if team.save
             render json: {
-                wrestlerID: params.wrestler.id,
+                wrestlerID: team_params[:wrestlerID],
                 drafted: true
             }, status: 200
         else
@@ -21,5 +21,9 @@ class Api::V1::TeamsController < ApplicationController
         end
     end
 
+    private
+    def team_params
+        params.permit(:teamID, :wrestlerID)
+    end
     
 end
